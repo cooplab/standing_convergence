3aa0ad99b942bdce53f57a5708f92355330870c1
peter <petrel.harp@gmail.com>
2015-01-15 09:42:26 -0800

    .

--- plot-2D-run.R
+++ plot-2D-run.R
@@ -3,15 +3,15 @@ source("sim-patchy-selection-fns.R")
 
 infile <- "sims/89011-r101-101-sb0.1-sm-0.01-N100-pophistory-run.Rdata"
 outfile <- gsub(".R[dD]ata",".png",infile)
-png( file=outfile, width=5*144, height=3*144, res=144, pointsize=10 )
+png( file=outfile, width=5*288, height=3*288, res=288, pointsize=10 )
 layout(matrix(1:6,nrow=2))
 par(mar=c(1,1,1,1)+.1)
-
 do.times <- c(6,8,10,12,14,16)
 stepsize <- 50
 for (k in do.times) {
     plotpop( pophist$pophist[,,,k], params=pophist$pop$params, plotlegend=FALSE, xlab='', ylab='', main=paste("t=",stepsize*k) )
 }
+dev.off()
 
 if (FALSE) {
 
