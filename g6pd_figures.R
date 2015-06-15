2c1fc8b232f02e455a7fa5135f834d2f97b84fe2
cooplab <gcoop79@gmail.com>
2015-06-14 19:36:51 -0700

    changes

--- g6pd_figures.R
+++ g6pd_figures.R
@@ -2,41 +2,9 @@ source("~/Dropbox/postdocs/Peter/standing_parallelism/standing-variation-fns.R")
 #install.packages("wesanderson")
 library("wesanderson")
 
-my.col<-wes.palette(5, "Darjeeling")  #wes.palette(2, "Royal1")
+my.col<-wes_palette(name="Darjeeling",n=5)
 
-
-######  Figure 1
-###################################################
-###################Char length as function of sb
-###################################################
-
- mu=1e-6; rhos=c(0.2,2); sd=0.05; sbvals=seq(0.00001,0.3,length=1600); 
-
-sigmas=c(100,50,10)
-
-layout(t(1:2))
-par(mar=c(4,4.3,2,0.5))
-for(j in 1:2){
-	rho=rhos[j]
-	if(j == 1) plot(sbvals,sbvals,type="n",ylim=c(100,1500),lwd=2,col=my.col[1],xlab=expression(s[b]),ylab=expression(list(chi, km)),cex.lab=1.5,cex.axis=1.4,main=expression(rho==0.2))
-	if(j == 2) plot(sbvals,sbvals,type="n",ylim=c(100,800),lwd=2,col=my.col[1],xlab=expression(s[b]),ylab="",cex.lab=1.5,cex.axis=1.4,main=expression(rho==2))
-	for(i in 1:3){
-	sigma=sigmas[i]; charlength.sb<-sapply(sbvals, function (sb) { charLength(mu,rho,sb,sd=sd,sigma)$value } )
-	 lines(sbvals,charlength.sb,lwd=2,col=my.col[i]); 
-	 
-	 charlength.new.only<-sapply(sbvals, function (sb) {  newCharLength(mu, rho, sb, sd=NA, sigma)$value})
-	 lines(sbvals,charlength.new.only,col=my.col[i],lwd=2,lty=2)  ##new mut. only
-	}
-	 ##contribution of standing var only, i.e. old variation
-	 charlength.standing.only<-sapply(sbvals, function (sb) {  oldCharLength(mu, rho, sb, sd=sd, sigma)$value})
-	 lines(sbvals,charlength.standing.only ,lwd=2,col="black",lty=3)
-	  if(j == 2) legend("topright",col=c("grey","grey","grey",my.col[1:3]),lwd=2,lty=c(2,3,1,rep(NA,3)),pch=c(NA,NA,NA,rep(19,3)),legend= as.expression( c("No Standing var.","No new mut.","Both",
-	 	lapply(sigmas , function(x){  {substitute( sigma==myS, list(myS=x) )} }))))
- 	}
-
-dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/G6PD_charlengths_sb.pdf")
-
-######  Figure 2
+######  Figure 4
 ###################################################
 ###################Char length as function of sd
 ###################################################
@@ -66,12 +34,44 @@ for(j in 1:2){
 	 	lapply(sigmas , function(x){  {substitute( sigma==myS, list(myS=x) )} }))))
  	}
  	
-dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/G6PD_charlengths.pdf")
+dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/G6PD_charlengths.pdf",useDingbat=FALSE)
+
+######  Figure 5
+###################################################
+###################Char length as function of sb
+###################################################
+
+ mu=1e-6; rhos=c(0.2,2); sd=0.05; sbvals=seq(0.00001,0.3,length=1600); 
+
+sigmas=c(100,50,10)
+
+layout(t(1:2))
+par(mar=c(4,4.3,2,0.5))
+for(j in 1:2){
+	rho=rhos[j]
+	if(j == 1) plot(sbvals,sbvals,type="n",ylim=c(100,1500),lwd=2,col=my.col[1],xlab=expression(s[b]),ylab=expression(list(chi, km)),cex.lab=1.5,cex.axis=1.4,main=expression(rho==0.2))
+	if(j == 2) plot(sbvals,sbvals,type="n",ylim=c(100,800),lwd=2,col=my.col[1],xlab=expression(s[b]),ylab="",cex.lab=1.5,cex.axis=1.4,main=expression(rho==2))
+	for(i in 1:3){
+	sigma=sigmas[i]; charlength.sb<-sapply(sbvals, function (sb) { charLength(mu,rho,sb,sd=sd,sigma)$value } )
+	 lines(sbvals,charlength.sb,lwd=2,col=my.col[i]); 
+	 
+	 charlength.new.only<-sapply(sbvals, function (sb) {  newCharLength(mu, rho, sb, sd=NA, sigma)$value})
+	 lines(sbvals,charlength.new.only,col=my.col[i],lwd=2,lty=2)  ##new mut. only
+	}
+	 ##contribution of standing var only, i.e. old variation
+	 charlength.standing.only<-sapply(sbvals, function (sb) {  oldCharLength(mu, rho, sb, sd=sd, sigma)$value})
+	 lines(sbvals,charlength.standing.only ,lwd=2,col="black",lty=3)
+	  if(j == 2) legend("topright",col=c("grey","grey","grey",my.col[1:3]),lwd=2,lty=c(2,3,1,rep(NA,3)),pch=c(NA,NA,NA,rep(19,3)),legend= as.expression( c("No Standing var.","No new mut.","Both",
+	 	lapply(sigmas , function(x){  {substitute( sigma==myS, list(myS=x) )} }))))
+ 	}
+
+dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/G6PD_charlengths_sb.pdf",useDingbat=FALSE) 
 
 
 
 
-#####Figure 3
+
+#####Figure 6
 ###############################################################
 ####Mean time standing as func. of sd & sb
 ###############################################################
@@ -117,9 +117,9 @@ for(j in 1:2){
 	 	lapply(sigmas , function(x){  {substitute( sigma==myS, list(myS=x) )} }))))
  	}
  	
-dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/G6PD_chartimes_sd_sb.pdf")
+dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/G6PD_chartimes_sd_sb.pdf",,useDingbat=FALSE)
 
-##### Figure 3 
+##### Figure 7
 ###############################################################
 ####Proportion of range & alleles that are standing
 ###############################################################
@@ -144,12 +144,12 @@ for(j in 1:2){
 	 	lines(sdvals,prop.number.sd,lty=2,col=my.col[i],lwd=2)
 	}
 }
-dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/G6PD_standing_var_proportion.pdf")
+dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/G6PD_standing_var_proportion.pdf",useDingbat=FALSE)
  
  #######################
- ###  Figure 4
+ ###  Figure 8
  ####effect of pleiotropy on standing variation
-  more.cols<-wes.palette(n=3,"FantasticFox")
+  more.cols<-wes_palette("FantasticFox",n=3)
   sd.1<-10^(-seq(-log10(0.05),5,length=100));
   plot(x=range(sd.1),y=c(0,1),type="n",log="x",xlab=expression(s[d1]),ylab=expression(p[1]),cex.lab=1.5,cex.axis=1.5)
  mu.2s<-c(1e-7,1e-6,1e-5)
@@ -176,7 +176,7 @@ mut.rates<-as.expression( lapply(c(-7,-6,-5) , function(x) {substitute( mu[2]== 
 my.exp<- as.expression(c(other.params,"Standing Var. Only",mut.rates))
  
  legend("topright",col=c(rep("grey",5),more.cols[1:3]),lwd=2,lty=c(1:4,rep(NA,4)),pch=c(rep(NA,4),1,rep(19,3)),legend=my.exp )
- dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/pleiotropy_calc.pdf")
+ dev.copy2pdf(file="~/Dropbox/postdocs/Peter/standing_parallelism/pleiotropy_calc.pdf",useDingbat=FALSE)
 
 
  
